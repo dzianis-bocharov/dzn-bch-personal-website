5d8eef59f601e123c4d6e7b77c98581dcc348489
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2021-12-25 06:39:13 -0500

    minor changes

--- lab/MainPage.php
+++ lab/MainPage.php
@@ -9,6 +9,22 @@
 	}
 ?>
 
+<?php
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else{
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+	if($ip = '::1') {
+		$ip = '127.0.0.1';
+	}
+	echo  "".$ip."";
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
