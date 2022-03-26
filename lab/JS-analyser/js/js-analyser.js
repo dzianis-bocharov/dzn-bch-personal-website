21de2703562e7ec506f4fd2bb66130c8825e0915
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-03-26 01:28:27 -0400

    add tabs for ierarchy scheme

--- lab/JS-analyser/js/js-analyser.js
+++ lab/JS-analyser/js/js-analyser.js
@@ -25,6 +25,7 @@ $('document').ready(function(){
         success: function (data) {
           ctx.font = "18px serif";
           const list_of_functions = javascript_array;
+          // alert(error_mesage);
           let x = 3;
           let w = 0;
           ctx.canvas.height = list_of_functions.length*70+20;
@@ -32,11 +33,13 @@ $('document').ready(function(){
           ctx.scale(z, z);
           $('.wideDiv').removeClass('btn-scroll-no').addClass('btn-scroll-yes');
           for (let i = 0; i < list_of_functions.length; i++) {
-            ctx.beginPath();
-            ctx.rect(20,20+ i * 70,175,50)
-            ctx.stroke();
-            ctx.font = "18px serif";
-            ctx.fillText(list_of_functions[w], 30, 50 + i * 70);
+            // ctx.beginPath();
+            // ctx.rect(20,20+ i * 70,175,50)
+            // ctx.stroke();
+            // ctx.font = "18px serif";
+            ctx.font = "16px serif";
+            // ctx.fillText(list_of_functions[w], 30, 50 + i * 70);
+            ctx.fillText(list_of_functions[w], 10, 20 + i * 20);
             w++;
           }
         }
--- lab/JS-analyser/js/test123.js
+++
@@ -1,3 +0,0 @@
-function dzianis1();
-function dzianis2();
-function dzianis3();
