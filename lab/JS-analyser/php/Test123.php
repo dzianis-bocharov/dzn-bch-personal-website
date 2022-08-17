f02a21da65cfdc75e3df3aa01cecf8522e722fa6
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-08-17 02:32:18 -0400

    add hot keys for tabs, launch, full screen

---
+++ lab/JS-analyser/php/Test123.php
@@ -0,0 +1,19 @@
+<?php
+
+    each(scalesOptions, (axisOptions, axisID) => {
+
+        axisOptions.id = axisID;
+        
+    });
+
+
+    $round_brackets_indicator = $curly_braces_indicator + preg_match_all("/{/",$file_lines_array[$x]) - preg_match_all("/}/",$file_lines_array[$x]);
+
+    $round_brackets_indicator++;
+
+    if
+
+    !
+
+
+?>
