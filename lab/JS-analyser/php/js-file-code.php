de02185d43e9d11685894bc9677be44602c13f48
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-08-19 02:06:54 -0400

    minor changes

--- lab/JS-analyser/php/js-file-code.php
+++ lab/JS-analyser/php/js-file-code.php
@@ -1,5 +1,8 @@
 <?php
-
-    echo 'Dzianis Bocharov';
-
+    $file = fopen("c:\Users\dzian\dzn-bch-personal-website\lab\JS-analyser\js\js-files-examples\chart.js", "r");
+        while(! feof($file)) {
+            $line = fgets($file);
+            echo $line. "<br>";
+        }
+    fclose($file);
 ?>
