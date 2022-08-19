f24370a2bf3388fd61063c620c3eec00716d8fa2
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-08-19 05:13:07 -0400

    minor changes

--- lab/JS-analyser/php/js-file-code.php
+++ lab/JS-analyser/php/js-file-code.php
@@ -2,7 +2,7 @@
     $file = fopen("c:\Users\dzian\dzn-bch-personal-website\lab\JS-analyser\js\js-files-examples\chart.js", "r");
         while(! feof($file)) {
             $line = fgets($file);
-            echo $line. "<br>";
+            echo '<pre>'.$line.'<pre>';
         }
     fclose($file);
 ?>
