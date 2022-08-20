5b33610e97b9c580afcb740c2d0ecfaaba11ea40
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-08-20 01:55:21 -0400

    minor changes

--- lab/js/constructor-of-class-chart.js
+++ lab/js/constructor-of-class-chart.js
@@ -59,126 +59,92 @@ constructor(item, userConfig) {
 }
 //----------левая сторона-----------------------------------------------------------------------------
 {
-    const me =
-    const initialCanvas =
-    const existingChart =
-    const options =
-    this.platform =
-    const context =
-    const canvas =
-    const height =
-    const width =
-    this.id =
-    this.ctx =
-    this.canvas =
-    
-    this.width = width;
-    this.height = height;
-    this._options = options;
-    this._aspectRatio = this.aspectRatio;
-    this._layers = [];
-    this._metasets = [];
-    this._stacks = undefined;
-    this.boxes = [];
-    this.currentDevicePixelRatio = undefined;
-    this.chartArea = undefined;
-    this._active = [];
-    this._lastEvent = undefined;
-    this._listeners = {};
-    this._responsiveListeners = undefined;
-    this._sortedMetasets = [];
-    this.scales = {};
-    this._plugins = new PluginService();
-    this.$proxies = {};
-    this._hiddenIndices = {};
-    this.attached = false;
-    this._animationsDisabled = undefined;
-    this.$context = undefined;
-    this._doResize = debounce(() => this.update('resize'), options.resizeDelay || 0);
-    instances[me.id] = me;
-    if (!context || !canvas) {
-      console.error("Failed to create chart: can't acquire context from the given item");
-      return;
-    }
-    animator.listen(me, 'complete', onAnimationsComplete);
-    animator.listen(me, 'progress', onAnimationProgress);
-    me._initialize();
 
+//const
+const me =
+
+//this
+this.any_word = 
+this.$proxies = 
+this._metasets =
+
+//слово с квадратной скобкой
+instances[me.id] =
         
 }
 //----------правая сторона----------------------------------------------------------------------------
 {
-    const me = this;
-    const config = this.config = new Config(userConfig);
-    const initialCanvas = getCanvas(item);
-    const existingChart = getChart(initialCanvas);
-    if (existingChart) {
-      throw new Error(
-        'Canvas is already in use. Chart with ID \'' + existingChart.id + '\'' +
-        ' must be destroyed before the canvas can be reused.'
-      );
-    }
-    const options = config.createResolver(config.chartOptionScopes(), me.getContext());
-    this.platform = new (config.platform || _detectPlatform(initialCanvas))();
-    const context = me.platform.acquireContext(initialCanvas, options.aspectRatio);
-    const canvas = context && context.canvas;
-    const height = canvas && canvas.height;
-    const width = canvas && canvas.width;
-    this.id = uid();
-    this.ctx = context;
-    this.canvas = canvas;
-    this.width = width;
-    this.height = height;
-    this._options = options;
-    this._aspectRatio = this.aspectRatio;
-    this._layers = [];
-    this._metasets = [];
-    this._stacks = undefined;
-    this.boxes = [];
-    this.currentDevicePixelRatio = undefined;
-    this.chartArea = undefined;
-    this._active = [];
-    this._lastEvent = undefined;
-    this._listeners = {};
-    this._responsiveListeners = undefined;
-    this._sortedMetasets = [];
-    this.scales = {};
-    this._plugins = new PluginService();
-    this.$proxies = {};
-    this._hiddenIndices = {};
-    this.attached = false;
-    this._animationsDisabled = undefined;
-    this.$context = undefined;
-    this._doResize = debounce(() => this.update('resize'), options.resizeDelay || 0);
-    instances[me.id] = me;
-    if (!context || !canvas) {
-      console.error("Failed to create chart: can't acquire context from the given item");
-      return;
-    }
-    animator.listen(me, 'complete', onAnimationsComplete);
-    animator.listen(me, 'progress', onAnimationProgress);
-    me._initialize();
-     if (me.attached) {
-       me.update();//закладка17
-    }
+
+//this
+= this;
+= this.aspectRatio;
+
+//одно слово / внутренние элементы / сопоставить с массивом $internal_elements
+= context;
+= canvas;
+= width;
+= height;
+= options;
+= me;
+
+//new
+= new PluginService();
+
+//внешние функции
+= getCanvas(item);
+= getChart(initialCanvas);
+= uid();
+
+= config.createResolver(config.chartOptionScopes(), me.getContext());
+= me.platform.acquireContext(initialCanvas, options.aspectRatio);
+= context && context.canvas;
+= canvas && canvas.height;
+= canvas && canvas.width;
+
+
+//квадратные скобки с пустотой
+= [];
+
+//undefined
+= undefined;
+
+//фигурные скобки
+= {};
+
+//false
+= false;
+
         
 }
 //----------проблемные случаи-------------------------------------------------------------------------
 {
 
-    const config = this.config = new Config(userConfig);//двойной знак равно
+const config = this.config = new Config(userConfig);//двойной знак равно
 
-    if (existingChart) {
-        throw new Error(
-          'Canvas is already in use. Chart with ID \'' + existingChart.id + '\'' +
-          ' must be destroyed before the canvas can be reused.'
-        );
-      }
+if (existingChart) {
+    throw new Error(
+        'Canvas is already in use. Chart with ID \'' + existingChart.id + '\'' +
+        ' must be destroyed before the canvas can be reused.'
+    );
+    }
+
+if (me.attached) {
+    me.update();
+}
+
+if (!context || !canvas) {
+    console.error("Failed to create chart: can't acquire context from the given item");
+    return;
+}
+
+animator.listen(me, 'complete', onAnimationsComplete);
+
+me._initialize();
+
+this._doResize = debounce(() => this.update('resize'), options.resizeDelay || 0);
+
+this.platform = new (config.platform || _detectPlatform(initialCanvas))();
   
-    if (me.attached) {
-        me.update();
-     }
-
 }
 //----------заметки-----------------------------------------------------------------------------------
 {    
@@ -186,4 +152,4 @@ constructor(item, userConfig) {
 }
 //----------------------------------------------------------------------------------------------------
 
-  }
+}
