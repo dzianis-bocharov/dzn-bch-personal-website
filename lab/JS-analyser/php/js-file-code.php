7275141751c32da5a49d34a7eb5c44bcfcec8a2d
dzianis-bocharov <fewd-nyc@dzianis-bocharov.com>
2022-08-25 04:14:17 -0400

    select a code of file by mouse without numbers of lines

--- lab/JS-analyser/php/js-file-code.php
+++ lab/JS-analyser/php/js-file-code.php
@@ -1,12 +1,12 @@
 <?php
-    echo '<code>';
+    echo '<pre><code>';
     $file = fopen("c:\Users\dzian\dzn-bch-personal-website\lab\JS-analyser\js\js-files-examples\chart.js", "r");
         $i=0;
         while(! feof($file)) {
             $i++;
             $line = fgets($file);
-            echo '<pre>'.$i.'   '.$line.'<pre>';
+            echo "<pre><span class='unselectable'>".$i."</span>".'  '.$line.'</pre>';
         }
     fclose($file);
-    echo '</code>';
+        echo '</code>';
 ?>
